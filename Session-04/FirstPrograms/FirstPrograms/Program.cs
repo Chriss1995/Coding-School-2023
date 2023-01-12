0c8433e19f8ae659ce7e71cb69c8428e4d367d92
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-01-12 16:09:46 +0200

    deleting and restarting

---
+++ Session-04/FirstPrograms/FirstPrograms/Program.cs
@@ -0,0 +1,17 @@
+﻿using FirstPrograms;
+using System.ComponentModel;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+ // 1. Write a C# program to print Hello and your name in the same line. 
+        string hello = "Hello";
+        string name = "Chrissanthi";
+
+        Console.WriteLine(hello + " " + name);
+        Console.ReadLine();
+
+    }
+}
