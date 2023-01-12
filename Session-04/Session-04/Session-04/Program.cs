181906668c0758634234993cb07407a94edb5be8
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-01-12 17:26:27 +0200

    First Program with exercises until 4

---
+++ Session-04/Session-04/Session-04/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Session_04;
+using System.ComponentModel;
+
+internal class ProgramOne
+{
+
+    private static void Main(string[] args)
+    {
+
+        // 1.Write a C# program to print Hello and your name in the same line
+        HelloName helloName = new HelloName();
+        Console.WriteLine(helloName.Get());
+
+        Console.ReadLine();
+
+        //2.Write a C# program to print the sum of two numbers and the division
+        //of those two numbers
+        int num1 = 10;
+        int num2 = 5;
+        Console.WriteLine(num1);
+        int sum = num1 + num2;
+        Console.WriteLine(sum);
+        int div = num1 / num2;
+        Console.WriteLine(div);
+        Console.ReadLine();
+
+        //3.Write a C# program to print the result of the specified operations
+        int result1 = -1 + 5 * 6;
+        Console.WriteLine(result1);
+        int result2 = 38 + 5 % 7;
+        Console.WriteLine(result2);
+        int result3= 14 + ((-3 * 6) /7);
+        Console.WriteLine(result3);
+        double result4 = 2 + (13 / 6) * 6 + Math.Sqrt(7);
+        Console.WriteLine(result4);
+        double result5 = (Math.Pow(6, 4) + Math.Pow(5, 7)) / (9 % 4);
+        Console.WriteLine(result5);
+        Console.ReadLine();
+
+        //Write a C# program that assigns an age (number) (for example 20) and
+        //a gender (string)(for example female) and displays something
+        //like: "You are female and look younger than 20
+        int age = 20;
+        string gender = "Frmale";
+        Console.WriteLine("You are " + gender + " and look younger than " + age);
+        Console.ReadLine();
+
+
+    }
+}
