6db7c38474188a56eae3a6871b26e77b409fb54f
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-01-16 17:56:00 +0200

    Schedule with constructors

--- Session-06/Session-06/Session-06/Schedule.cs
+++ Session-06/Session-06/Session-06/Schedule.cs
@@ -14,7 +14,28 @@ namespace Session_06
         public DateTime Callendar { get; set; }
 
         public Schedule() { }
+        public Schedule(Guid id) { ID = id; }
+        public Schedule(Guid id, Guid courseID)
+        {
+            ID = id;
+            CourseID = courseID;
+        }
+        public Schedule(Guid  id, Guid courseID, Guid proffessorID)
+        {
+            ID = id;
+            CourseID = courseID;
+            ProffessorID = proffessorID;
 
+        }
+        public Schedule(
+            Guid id,  Guid courseID, Guid proffessorID, DateTime callendar)
+        {
+            ID = id;
+            CourseID = courseID;
+            ProffessorID = proffessorID;
+            Callendar = Callendar;
+
+        }
     }
 
 }
