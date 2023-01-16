42c469b7bb356723e674086f8d778f2b070943aa
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-01-16 17:48:13 +0200

    2 last classes

---
+++ Session-06/Session-06/Session-06/Institute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_06
+{
+    public class Institute
+    { public Guid ID { get; set; }
+    public string? Name { get; set; }
+    public int YearsInService { get; set; }
+        public Institute() { }
+        public void GetName() { }
+        public void SetName(string name) { }
+    }
+}
---
+++ Session-06/Session-06/Session-06/University.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_06
+{
+    public class University : Institute
+    {public Student[]? Student { get; set; }
+     public Course[]? Course { get; set; }
+     public Grade[]? Grades { get; set; }
+    public Schedule[]? ScheduledCourse { get; set; }
+    public University() { }
+    public void GetStudents() { }
+    public void GetCourses() { }
+     public void GetGrades() { }
+     public void SetSchedule(Course CourseID, Professor ProffessorID, DateTime dateTime) { }
+
+    }
+}
