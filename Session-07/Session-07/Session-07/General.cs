5bbaae17108e8c8cfd1f4ec233e77f5f9ccdecb5
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-01-17 15:20:24 +0200

    Pushing the start of the problem

---
+++ Session-07/Session-07/Session-07/General.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_07
+{
+    public enum ActionEnum{
+    Convert,
+    Uppercase,
+    Reverse}
+    
+    public class Message
+    {
+
+    }
+    public class MessageLogger
+    {
+
+    }
+    public class ActionRequest
+    {
+
+    }
+    public class ActionResponse
+    {
+
+    }
+    public class ActionResolver
+    {
+        //Properties
+        public MessageLogger Logger { get; set; }
+
+        //Constructors
+        public ActionResolver()
+        {
+
+        }
+        //Methods
+        public ActionResponse Execute(ActionRequest request)
+        {
+
+
+            return null;
+        }
+    }
+}
