b88e2f2862362d5f88f33afc0bb16d1ba14818c3
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-01-17 17:02:15 +0200

    saving as I finished the methods

--- Session-07/Session-07/Session-07/General.cs
+++ Session-07/Session-07/Session-07/General.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Diagnostics.SymbolStore;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -76,9 +78,52 @@ namespace Session_07
         //Methods
         public ActionResponse Execute(ActionRequest request)
         {
-
-
+            string Output;
+            switch (ActionEnum)
+            {
+                case ActionEnum.Convert:
+                    Output = Conversion(request);
+                    break;
+                case ActionEnum.Uppercase:
+                    Output = Uppercase(request);
+                    break;
+                case ActionEnum.Reverse:
+                    Output = Reversion(request);
+                    break;
+                default:
+                    Console.WriteLine("Error Message");
+                    break;
+            }
             return null;
         }
+    }
+    public string Reversion(string Input)
+    {
+        char[] charArray = Input.ToCharArray();
+        string reversedString = string.Empty;
+        for (int i = charArray.Length - 1; i > -1; i--)
+        {
+            reversedString += charArray[i];
+        }
+        return reversedString;
+    }
+    public string Conversion(string Input)
+    {
+        bool Convertedval =  Decimal.TryParse(out Input);
+        if(Convertedval)
+        {
+            int number = int.Parse(Input);
+            string result = string.Empty();
+            for(int i= 0; number >0; i++)
+            {
+                result = number % 2 + result;
+                number = number / 2;
+            }
+        }
+
+    }
+    public string Upppercase(string Input)
+    {
+        string Outstring = Input.ToUpper();
     }
 }
