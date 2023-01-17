a49b0ce1225bb82273ed0f30ed1c74406da76da5
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-01-17 15:49:56 +0200

    Starting Class Diagram

--- Session-07/Session-07/Session-07/General.cs
+++ Session-07/Session-07/Session-07/General.cs
@@ -11,22 +11,57 @@ namespace Session_07
     Convert,
     Uppercase,
     Reverse}
-    
-    public class Message
-    {
 
+    public class Message
+    {//Properties
+        public Guid ID { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string text_message { get; set; }
+        // Constractor
+        public Message() { }
     }
     public class MessageLogger
-    {
+    {// Properties
+        public Message[] Messages { get; set; }
+        //Contstructor
+        public MessageLogger()
+        { }
+        // Methods
+        public void ReadAll()
+        {
+
+        }
+        public void Clear()
+        {
+
+        }
+        public Message Write()
+        {
+
+        }
 
     }
     public class ActionRequest
-    {
+    {//Properties
+       public Guid RequestID { get; set; }
+        public string Input { get; set; }
+        public ActionEnum Action { get; set; }
+    // Constructors
+     public ActionRequest() {
+        }
 
-    }
+   }
     public class ActionResponse
-    {
+    {//Properties
+        public Guid ResponseID { get; set; }
+        public Guid RequestID { get; set; }
+        public String Output { get; set; }
 
+        //Constructors
+        public ActionResponse()
+        {
+
+        }
     }
     public class ActionResolver
     {
