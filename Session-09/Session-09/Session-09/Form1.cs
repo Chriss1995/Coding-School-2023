578d5853ca5f0b4369b6ac42c43966460801e9d6
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-01-19 16:34:21 +0200

    First button

--- Session-09/Session-09/Session-09/Form1.cs
+++ Session-09/Session-09/Session-09/Form1.cs
@@ -2,6 +2,19 @@ namespace Session_09
 {
     public partial class Form1 : Form
     {
+        private decimal? _value1 = null;
+        private decimal? _value2 = null;
+        private decimal? _result = null;
+        private CalcOperation _calcOperation;
+        enum CalcOperation
+        {
+            Addition, 
+            Subtraction, 
+            Multiplication, 
+            Division,
+            Power,
+            Squared_root
+        }
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +22,22 @@ namespace Session_09
 
         private void btn1_Click(object sender, EventArgs e)
         {
-           
+           if (_result != null)
+            {
+                txtDisplay.Text = string.Empty;
+                _value1 = null;
+                _value2 = null;
+                _result= null;
+            }
+            txtDisplay.Text += " 2";
+            if (_value1 == null)
+            {
+                _value1 = 1;
+            }
+            else
+            {
+                _value2 = 1;
+            }
         }
     }
 }
