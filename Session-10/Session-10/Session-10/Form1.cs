2619fd2d634006c206fb40ef1ff9b446361d6b78
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-01-20 17:38:47 +0200

    Start Point 2

--- Session-10/Session-10/Session-10/Form1.cs
+++ Session-10/Session-10/Session-10/Form1.cs
@@ -1,12 +1,23 @@
+using UniversityLib;
+
 namespace Session_10
 {
     public partial class Form1 : Form
     {
+        List<Student> students;
+
         public Form1()
         {
             InitializeComponent();
         }
-     
+        private void PopulateStudent()
+        {
+            students = new List<Student>();
+
+        }
+
+
+
 
 
     }
