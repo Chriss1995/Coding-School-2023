c849e8f1827324f3de6a0495778b9a7482293d16
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-01-20 17:47:34 +0200

    Students

--- Session-10/Session-10/Session-10/Form1.cs
+++ Session-10/Session-10/Session-10/Form1.cs
@@ -5,6 +5,7 @@ namespace Session_10
     public partial class Form1 : Form
     {
         List<Student> students;
+        List<Course> courses;
 
         public Form1()
         {
@@ -13,6 +14,20 @@ namespace Session_10
         private void PopulateStudent()
         {
             students = new List<Student>();
+            Student student1 = new Student()
+            {
+                name = "Chrissanthi",
+                age = 28,
+                RegistrationNumber = 10050
+            };
+            students.Add(student1);
+            Student student2 = new Student()
+            {
+                name = "Lydia",
+                age = 18,
+                RegistrationNumber = 100060
+            };
+            students.Add(student2);
 
         }
 
