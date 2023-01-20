b84e204f4da01a861f1537995a6064c2eb39646a
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-01-20 18:07:12 +0200

    Designing Starting

--- Session-10/Session-10/Session-10/Form1.Designer.cs
+++ Session-10/Session-10/Session-10/Form1.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.grvStudents = new System.Windows.Forms.DataGridView();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnLoad = new System.Windows.Forms.Button();
+            this.gvrGrades = new System.Windows.Forms.DataGridView();
+            this.gvrChedule = new System.Windows.Forms.DataGridView();
+            this.gdvCourses = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.grvStudents)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvrGrades)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvrChedule)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gdvCourses)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // grvStudents
@@ -46,7 +55,7 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(26, 361);
+            this.btnSave.Location = new System.Drawing.Point(26, 440);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(115, 59);
             this.btnSave.TabIndex = 1;
@@ -56,7 +65,7 @@
             // 
             // btnLoad
             // 
-            this.btnLoad.Location = new System.Drawing.Point(158, 361);
+            this.btnLoad.Location = new System.Drawing.Point(147, 440);
             this.btnLoad.Name = "btnLoad";
             this.btnLoad.Size = new System.Drawing.Size(115, 59);
             this.btnLoad.TabIndex = 2;
@@ -64,17 +73,54 @@
             this.btnLoad.UseVisualStyleBackColor = true;
             this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
             // 
+            // gvrGrades
+            // 
+            this.gvrGrades.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvrGrades.Location = new System.Drawing.Point(26, 165);
+            this.gvrGrades.Name = "gvrGrades";
+            this.gvrGrades.RowHeadersWidth = 51;
+            this.gvrGrades.RowTemplate.Height = 29;
+            this.gvrGrades.Size = new System.Drawing.Size(338, 134);
+            this.gvrGrades.TabIndex = 3;
+            // 
+            // gvrChedule
+            // 
+            this.gvrChedule.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvrChedule.Location = new System.Drawing.Point(26, 305);
+            this.gvrChedule.Name = "gvrChedule";
+            this.gvrChedule.RowHeadersWidth = 51;
+            this.gvrChedule.RowTemplate.Height = 29;
+            this.gvrChedule.Size = new System.Drawing.Size(755, 134);
+            this.gvrChedule.TabIndex = 4;
+            // 
+            // gdvCourses
+            // 
+            this.gdvCourses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gdvCourses.Location = new System.Drawing.Point(392, 165);
+            this.gdvCourses.Name = "gdvCourses";
+            this.gdvCourses.RowHeadersWidth = 51;
+            this.gdvCourses.RowTemplate.Height = 29;
+            this.gdvCourses.Size = new System.Drawing.Size(389, 134);
+            this.gdvCourses.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 496);
+            this.Controls.Add(this.gdvCourses);
+            this.Controls.Add(this.gvrChedule);
+            this.Controls.Add(this.gvrGrades);
             this.Controls.Add(this.btnLoad);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.grvStudents);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.grvStudents)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvrGrades)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvrChedule)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gdvCourses)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -84,5 +130,9 @@
         private DataGridView grvStudents;
         private Button btnSave;
         private Button btnLoad;
+        private DataGridView gvrGrades;
+        private DataGridView gvrChedule;
+        private DataGridView gdvCourses;
+        private BindingSource bindingSource1;
     }
 }
