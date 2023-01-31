35b45c87be31c19ca2d139ffa79d32bfa55930c9
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-01-31 17:33:00 +0200

    Stage 5 database stage 1

---
+++ Session-15/Session_15/CF.Orm/Configuration/EmployeeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CoffeeshopModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CF.Orm.Configuration
+{
+    public class EmployeeConfiguration: IEntityTypeConfiguration<Employee>
+    { public void Configure(EntityTypeBuilder<Employee> builder) {
+            builder.ToTable("Employee");
+            builder.HasKey(Employee => Employee.ID);
+            builder.Property(Employee => Employee.ID).ValueGeneratedOnAdd();
+            builder.Property(Employee => Employee.Name).HasMaxLength(50);
+            builder.Property(Employee => Employee.Surname).HasMaxLength(50);
+            builder.Property(Employee=> Employee.TypeofEmployee).HasConversion<string>();
+            builder.Property(Employee => Employee.SalaryPerMonth).HasConversion<decimal>();
+                }
+    }
+}
---
+++ Session-15/Session_15/CF.Orm/Context/EmployeeContext.cs
@@ -0,0 +1,21 @@
+﻿using CF.Orm.Configuration;
+using CoffeeshopModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace CF.Orm.Context
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TodoDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
