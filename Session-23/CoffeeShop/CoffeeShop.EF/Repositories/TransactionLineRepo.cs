bc5c0c46650a130bf6be366656f56b0b9bba4e32
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-08 20:35:21 +0200

    First Repo and New Project

---
+++ Session-23/CoffeeShop/CoffeeShop.EF/Repositories/TransactionLineRepo.cs
@@ -0,0 +1,17 @@
+﻿using CoffeeShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.EF.Repositories
+{
+    public class TransactionLineRepo
+    {
+        public void Add(TransactionLine)
+        {
+           
+        }
+    }
+}
