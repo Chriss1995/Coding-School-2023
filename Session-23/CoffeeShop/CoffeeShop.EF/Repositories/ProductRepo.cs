a00f81af5f72962d4b24a58f0f6b3e878c1f0cea
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-08 22:52:46 +0200

    Stage 4 Add ProductRepo

---
+++ Session-23/CoffeeShop/CoffeeShop.EF/Repositories/ProductRepo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoffeeShop.Model;
+using CoffeeShop.EF.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.EF.Repositories
+{
+    public class ProductRepo
+    {
+        public void Add(Product product)
+        {
+            using var context = new CoffeeShopDbContext();
+            context.Add(product);
+            context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            using var context = new CoffeeShopDbContext();
+            var dbProduct = context.Products.Where(Product=> Product.Id == id).SingleOrDefault();
+            if (dbProduct is null)
+                return;
+            context.Products.Remove(dbProduct);
+            context.SaveChanges();
+        }
+        public void Update(int id, Product product)
+        {
+            using var context = new CoffeeShopDbContext();
+            var dbProduct = context.Products.Where(Product=> Product.Id == id).SingleOrDefault();
+            if (dbProduct is null) return;
+            dbProduct.Code = product.Code;
+            dbProduct.Description= product.Description;
+            dbProduct.Price = product.Price;
+            product.Cost = product.Cost;
+            product.ProductCategory = product.ProductCategory;
+
+        }
+    }
+}
