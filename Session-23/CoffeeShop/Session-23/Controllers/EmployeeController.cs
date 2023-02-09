bae2e15fa31066aa8a344df989557df3b46d4bd0
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-09 22:59:52 +0200

    Stage 8

---
+++ Session-23/CoffeeShop/Session-23/Controllers/EmployeeController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CoffeeShop.EF.Repositories;
+using CoffeeShop.Model;
+using Session_23.Models.Employees;
+
+namespace Session_23.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeRepo _employeeRepo;
+        public EmployeeController(EmployeeRepo employee)
+        {
+            _employeeRepo = employee;
+        }
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            var employees = _employeeRepo.GetAll();
+            var EmployeeList = employees.ToList(); 
+            return View();
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int? id)
+        {   
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var employees = _employeeRepo.GetByID(id.Value);
+            if (employees == null)
+            {
+                return NotFound();
+            }
+            var viewEmployee = new EmployeesDetailsDto
+            {
+                Id = employees.Id,
+                Name = employees.Name,
+                Surname = employees.Surname,
+                SalaryPerMoth = employees.SalaryPerMonth,
+                EmployeeType= employees.EmployeeType
+            };
+            return View(model:viewEmployee);
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Session-23/CoffeeShop/Session-23/Models/Employees/EmployeesDetailsDto.cs
@@ -0,0 +1,17 @@
+﻿using CoffeeShop.Model.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+
+namespace Session_23.Models.Employees
+{
+    public class EmployeesDetailsDto
+    {
+        public int Id { get; set; }
+        [Display (Name = "Name") ]
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public int SalaryPerMoth { get; set; }
+        public EmployeeType EmployeeType { get; set; }
+    }
+}
