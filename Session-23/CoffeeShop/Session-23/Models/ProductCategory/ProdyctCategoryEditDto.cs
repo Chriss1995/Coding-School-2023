b7153e8a7a3ca9de8aae85f000f41a37f72595bd
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-11 11:09:01 +0200

    Stage 16 Product Category MVC

---
+++ Session-23/CoffeeShop/Session-23/Models/ProductCategory/ProdyctCategoryEditDto.cs
@@ -0,0 +1,16 @@
+﻿using CoffeeShop.Model.Enums;
+using CoffeeShop.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace Session_23.Models.ProductCategory
+{
+    public class ProdyctCategoryEditDto
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public ProductType ProductType { get; set; }
+        public List<Transaction>? Transactions { get; set; }
+
+    }
+}
--- Session-23/CoffeeShop/Session-23/Program.cs
+++ Session-23/CoffeeShop/Session-23/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<EmployeeRepo>();
+builder.Services.AddScoped<ProductCategoryRepo>();
 
 var app = builder.Build();
 
