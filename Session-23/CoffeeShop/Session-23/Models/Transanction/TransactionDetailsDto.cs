79bf75cc8902df08f16b74cec602116a3452cc4c
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-12 14:10:43 +0200

    Transaction Dtos

---
+++ Session-23/CoffeeShop/Session-23/Models/Transanction/TransactionDetailsDto.cs
@@ -0,0 +1,18 @@
+﻿using CoffeeShop.Model;
+using CoffeeShop.Model.Enums;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Session_23.Models.Transanction
+{
+    public class TransactionDetailsDto
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalPrice { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public int CustomerId { get; set; }
+        public List<SelectListItem> Customer { get; set; } =  new List<SelectListItem>();
+        public int EmployeeId { get; set; }
+        public List<SelectListItem> Employee { get; set; } = new List<SelectListItem>();
+    }
+}
