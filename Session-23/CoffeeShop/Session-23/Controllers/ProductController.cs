2ed76fc2ae1fd2a17425aaa5b3c30655f5d566b2
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-13 18:46:34 +0200

    Controller Edit Product

--- Session-23/CoffeeShop/Session-23/Controllers/ProductController.cs
+++ Session-23/CoffeeShop/Session-23/Controllers/ProductController.cs
@@ -62,16 +62,48 @@ namespace Session_23.Controllers
         // GET: ProductController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var dbProduct = _productRepo.GetByID(id);
+            if(dbProduct == null)
+            {
+                return NotFound();
+            }
+            var ProductDto = new ProductEditDto()
+            {
+                Code = dbProduct.Code,
+                Description= dbProduct.Description,
+                Price= dbProduct.Price,
+                Cost= dbProduct.Cost
+            };
+            var ProductCategories = _productCategoryRepo.GetAll();
+            foreach(var ProductCategory in ProductCategories)
+            {
+                ProductDto.ProductCategory.Add(new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem(ProductCategory.Id.ToString(), ProductCategory.Id.ToString()));
+            }
+            return View(ProductDto);
         }
 
         // POST: ProductController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, ProductEditDto product)
         {
             try
             {
+                if(!ModelState.IsValid)
+                {
+                    return View();
+                }
+                var dbProduct = _productRepo.GetByID(id);
+                if(dbProduct == null)
+                {
+                    return NotFound();
+                }
+                dbProduct.Code = product.Code;
+                dbProduct.Description = product.Description;
+                dbProduct.Price = product.Price;
+                dbProduct.Cost = product.Cost;
+                dbProduct.ProductCategoryId = product.ProductCategoryId;
+                _productRepo.Update(id, dbProduct);
                 return RedirectToAction(nameof(Index));
             }
             catch
