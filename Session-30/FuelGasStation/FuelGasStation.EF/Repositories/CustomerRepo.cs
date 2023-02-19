428fd2e3f1a6cc6eca80c1205436e497d7bffc18
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-19 14:58:40 +0200

    CustomerRepo

---
+++ Session-30/FuelGasStation/FuelGasStation.EF/Repositories/CustomerRepo.cs
@@ -0,0 +1,63 @@
+﻿using FuelGasStation.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FuelGasStation.EF.Context;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+
+namespace FuelGasStation.EF.Repositories
+{
+    public class CustomerRepo : IEntityRepo<Customer>
+    {
+        public void Add(Customer entity)
+        {
+            using var context = new FuelGasStationDbContext();
+            if (entity.Id != 0)
+            {
+                throw new ArgumentException("Given entity should not have Id set", nameof(entity));
+            }
+            context.Customers.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            using var context = new FuelGasStationDbContext();
+            var dbCustomer = context.Customers.SingleOrDefault(Customer => Customer.Id == id);
+            if (dbCustomer is null)
+            {
+                throw new KeyNotFoundException($"Given id '{id}' was not found in database");
+            }
+            context.Customers.Remove(dbCustomer);
+            context.SaveChanges();
+        }
+
+        public IList<Customer> GetAll()
+        {
+            using var context = new FuelGasStationDbContext();
+            return context.Customers.ToList();
+        }
+       
+        public Customer? GetById(int id)
+        {
+            using var context = new FuelGasStationDbContext();
+            return context.Customers.SingleOrDefault(Customer => Customer.Id == id);
+        }
+
+        public void Update(int id, Customer entity)
+        {
+            using var context = new FuelGasStationDbContext();
+            var dbCustomer = context.Customers.SingleOrDefault(Customer => Customer.Id == id);
+            if(dbCustomer is null)
+            {
+                throw new KeyNotFoundException($"Given id '{id} was not found in database'");
+            }
+            dbCustomer.Name = entity.Name;
+            dbCustomer.Surname = entity.Surname;
+            dbCustomer.CardNumber = entity.CardNumber;
+            context.SaveChanges();
+        }
+    }
+}
