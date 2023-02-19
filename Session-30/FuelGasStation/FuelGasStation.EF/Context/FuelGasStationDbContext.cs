61089e0cea8953c0c313c1a47b11788367be573e
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-19 11:23:24 +0200

    Context - Customer Configurations

---
+++ Session-30/FuelGasStation/FuelGasStation.EF/Context/FuelGasStationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FuelGasStation.EF.Configurations;
+using FuelGasStation.Model;
+
+namespace FuelGasStation.EF.Context
+{
+    public class FuelGasStationDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void  OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FuelGasStation;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
