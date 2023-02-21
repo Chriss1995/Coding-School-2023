bc09ff2b975f73691df312848200af569394483c
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-21 14:24:24 +0200

    Customer Controller New

---
+++ Session-30/FuelGasStation/FuelGasStation.Blazor/Server/Controllers/CustomerController.cs
@@ -0,0 +1,70 @@
+﻿using FuelGasStation.EF.Repositories;
+using FuelGasStation.Model;
+using FuelGasStation.Blazor.Shared.Customer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace FuelGasStation.Blazor.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        readonly IEntityRepo<Customer> _customerRepo;
+
+        public CustomerController(IEntityRepo<Customer> customerRepo)
+        {
+            _customerRepo = customerRepo;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<CustomerListDto>> Get()
+        {
+            var result = _customerRepo.GetAll();
+            return result.Select(Customer => new CustomerListDto
+            {
+                Id = Customer.Id,
+                Name = Customer.Name,
+                Surname = Customer.Surname,
+                CardNumber = Customer.CardNumber,
+            });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<CustomerEditDto> GetById(int id)
+        {
+            var result = _customerRepo.GetById(id);
+            return new CustomerEditDto
+            {
+                Id = result.Id,
+                Name = result.Name,
+                Surname = result.Surname,
+                CardNumber = result.CardNumber
+            };
+        }
+
+        [HttpPost]
+        public async Task Post(CustomerEditDto customer)
+        {
+            var newCustomer = new Customer(customer.Name, customer.Surname, customer.CardNumber);
+            _customerRepo.Add(newCustomer);
+        }
+
+        [HttpPut]
+        public async Task Put(CustomerEditDto customer)
+        {
+            var itemToUpdate = _customerRepo.GetById(customer.Id);
+            itemToUpdate.Name = customer.Name;
+            itemToUpdate.Surname = customer.Surname;
+            itemToUpdate.CardNumber = customer.CardNumber;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            _customerRepo.Delete(id);
+            return Ok();
+        }
+    }
+}
