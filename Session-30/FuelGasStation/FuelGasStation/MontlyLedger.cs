5dabed4ad57ce26979e684df6a545db1a6374a06
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-22 22:52:46 +0200

    Monthly Ledger Class

---
+++ Session-30/FuelGasStation/FuelGasStation/MontlyLedger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelGasStation.Model
+{
+    public class MontlyLedger
+    {
+        public MontlyLedger(int year, int month, IList<Transaction> transactions, IList<Employee> employees)
+        {
+            Year = year;
+            Month = month;
+            Transactions = transactions;
+            Employees = employees;
+        }
+
+        public decimal Exenses { get; set; }
+        public decimal Income { get; set; }
+        public decimal Total { get; set; }
+        public IList<Transaction> Transactions { get; set; } = new List<Transaction>();
+        public IList<Employee> Employees { get; set; } = new List<Employee>();
+        public int Year { get; set; }
+        public int Month { get; set; }
+    
+    }
+}
