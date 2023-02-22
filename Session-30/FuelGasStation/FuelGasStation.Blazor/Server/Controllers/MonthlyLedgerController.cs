ffd2d3003717112a1b946bc30bd26e640c3fd4cf
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-22 23:33:08 +0200

    Monthly Ledger start Controller and View

---
+++ Session-30/FuelGasStation/FuelGasStation.Blazor/Server/Controllers/MonthlyLedgerController.cs
@@ -0,0 +1,63 @@
+﻿using FuelGasStation.EF.Repositories;
+using FuelGasStation.Model;
+using FuelGasStation.Blazor.Shared.MonthlyLedger;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace FuelGasStation.Blazor.Server.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class MonthlyLedgerController : ControllerBase
+    {
+        private IEntityRepo<Transaction> _transRepo;
+        private IEntityRepo<Employee> _employeeRepo;
+
+        public MonthlyLedgerController(IEntityRepo<Transaction> transRepo, IEntityRepo<Employee> employeeRepo)
+        {
+            _transRepo = transRepo;
+            _employeeRepo = employeeRepo;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<MonthlyLedgerDto>> Get()
+        {
+            List<MonthlyLedgerDto> monthlyLedgers = new();
+            var transactions = _transRepo.GetAll().ToList();
+            var employees = _employeeRepo.GetAll().ToList();
+
+            var rent = 5000;
+            decimal totalTransactions = 0;
+            decimal totalSalaryEmployees = 0;
+            
+            foreach ( var transaction in transactions ) 
+            {
+                totalTransactions += transaction.TotalValue;
+            }
+
+            foreach(var employee in employees )
+            {
+                totalSalaryEmployees += employees.Select(employee => employee.SalaryPerMonth)
+                                                 .SingleOrDefault();
+            }
+
+            var groupedTransactions = transactions.GroupBy(transactions => new { transactions.DateTime.Year, transactions.DateTime.Month })
+               .Select(grouped => new MonthlyLedgerDto
+               {
+                   Year = grouped.Key.Year,
+                   Month = grouped.Key.Month,
+                   Income = totalTransactions,
+                   Expenses = totalSalaryEmployees + rent,
+                   Total = totalTransactions - (totalSalaryEmployees + rent)
+               });
+            
+            foreach(var grouped in groupedTransactions )
+            {
+                monthlyLedgers.Add( grouped );
+            }
+            
+            return groupedTransactions;
+        }
+    }
+}
---
+++ Session-30/FuelGasStation/FuelGasStation.Blazor/Shared/MonthlyLedger/MonthlyLedgerDto.cs
@@ -0,0 +1,21 @@
+﻿using FuelGasStation.Blazor.Shared.Employee;
+using FuelGasStation.Blazor.Shared.Transaction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelGasStation.Blazor.Shared.MonthlyLedger
+{
+    public class MonthlyLedgerDto
+    {
+        public decimal Expenses { get; set; }
+        public decimal Income { get; set; }
+        public decimal Total { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public List<TransactionListDto> Transactions { get; set; } =new List<TransactionListDto>();
+        public List<EmployeeListDto> Employees { get; set; } =new List<EmployeeListDto>();
+    }
+}
