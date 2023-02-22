f3baa6c4d443a8be32130c231895305e599e72bb
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-22 16:05:19 +0200

    Transaction Line and Transcations Dtos

---
+++ Session-30/FuelGasStation/FuelGasStation.Blazor/Shared/Transaction/TransactionListDto.cs
@@ -0,0 +1,25 @@
+﻿using FuelGasStation.Blazor.Shared.Customer;
+using FuelGasStation.Blazor.Shared.Employee;
+using FuelGasStation.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelGasStation.Blazor.Shared.Transaction
+{
+    public class TransactionListDto
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public PaymentMetod PaymentMethod { get; set; }
+        public decimal TotalValue { get; set; }
+        public int CustomerID { get; set; }
+        public string CustomerSurname { get; set; } = null!;
+        public List<CustomerListDto> Cutomers { get; set; } = new();
+        public int EmployeeID { get; set; }
+        public string EmployeeSurname { get; set; } = null!;
+        public List<EmployeeListDto> Employees { get; set; } = new();
+    }
+}
---
+++ Session-30/FuelGasStation/FuelGasStation.Blazor/Shared/TransactionLine/TransactionLineListDto.cs
@@ -0,0 +1,26 @@
+﻿using FuelGasStation.Blazor.Shared.Item;
+using FuelGasStation.Blazor.Shared.Transaction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelGasStation.Blazor.Shared.TransactionLine
+{
+    public class TransactionLineListDto
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public decimal ItemPrice { get; set; }
+        public decimal NetValue { get; set; }
+        public decimal PercentDiscount { get; set; }
+        public decimal DiscountValue { get; set; }
+        public decimal TotalValue { get; set; }
+        public int TransactionID { get; set; }
+        public List<TransactionListDto> Transactions { get; set; } = new();
+        public string ItemCode { get; set; } = null!;
+        public int ItemID { get; set; }
+        public List<ItemListDto> Items { get; set; } = new();
+    }
+}
