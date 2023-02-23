a1ceea3d5b39ad2673a876caa5210e07edb9b5e5
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-23 22:10:42 +0200

    start of Customer Form

--- Session-30/FuelGasStation/FuelGasStation.WinForm/Form1.Designer.cs
+++ Session-30/FuelGasStation/FuelGasStation.WinForm/Form1.Designer.cs
@@ -29,11 +29,106 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.customerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.coId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSurname = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colCardNumber = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // customerBindingSource
+            // 
+            this.customerBindingSource.DataSource = typeof(FuelGasStation.Model.Customer);
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Location = new System.Drawing.Point(72, 22);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(625, 268);
+            this.gridControl1.TabIndex = 1;
+            this.gridControl1.UseEmbeddedNavigator = true;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.coId,
+            this.colName,
+            this.colSurname,
+            this.colCardNumber});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // coId
+            // 
+            this.coId.Caption = "id";
+            this.coId.FieldName = "Id";
+            this.coId.MinWidth = 25;
+            this.coId.Name = "coId";
+            this.coId.Width = 94;
+            // 
+            // colName
+            // 
+            this.colName.Caption = "Name";
+            this.colName.FieldName = "Name";
+            this.colName.MinWidth = 25;
+            this.colName.Name = "colName";
+            this.colName.Visible = true;
+            this.colName.VisibleIndex = 1;
+            this.colName.Width = 94;
+            // 
+            // colSurname
+            // 
+            this.colSurname.Caption = "Surname";
+            this.colSurname.FieldName = "Surname";
+            this.colSurname.MinWidth = 25;
+            this.colSurname.Name = "colSurname";
+            this.colSurname.Visible = true;
+            this.colSurname.VisibleIndex = 2;
+            this.colSurname.Width = 94;
+            // 
+            // colCardNumber
+            // 
+            this.colCardNumber.Caption = "CardNumber";
+            this.colCardNumber.FieldName = "CardNumber";
+            this.colCardNumber.Fixed = DevExpress.XtraGrid.Columns.FixedStyle.Left;
+            this.colCardNumber.MinWidth = 25;
+            this.colCardNumber.Name = "colCardNumber";
+            this.colCardNumber.ShowButtonMode = DevExpress.XtraGrid.Views.Base.ShowButtonModeEnum.ShowAlways;
+            this.colCardNumber.Visible = true;
+            this.colCardNumber.VisibleIndex = 0;
+            this.colCardNumber.Width = 94;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private BindingSource customerBindingSource;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn coId;
+        private DevExpress.XtraGrid.Columns.GridColumn colName;
+        private DevExpress.XtraGrid.Columns.GridColumn colSurname;
+        private DevExpress.XtraGrid.Columns.GridColumn colCardNumber;
     }
 }
--- Session-30/FuelGasStation/FuelGasStation.WinForm/Form1.cs
+++ Session-30/FuelGasStation/FuelGasStation.WinForm/Form1.cs
@@ -1,10 +1,39 @@
+using FuelGasStation.Blazor.Shared.Customer;
+using System.Net.Http;
+using System.Net.Http.Json;
 namespace FuelGasStation.WinForm
 {
     public partial class Form1 : Form
     {
+
+
+        private readonly HttpClient client;
         public Form1()
         {
             InitializeComponent();
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7222/");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _ = SetControlProperties();
+
+        }
+        private async Task SetControlProperties()
+        {
+            customerBindingSource.DataSource = await GetCustomers();
+            gridControl1.DataSource = customerBindingSource;
+        }
+        private async Task<List<CustomerListDto?>> GetCustomers()
+        {
+            var response = await client.GetFromJsonAsync<List<CustomerListDto?>>("customer");
+            return response.ToList();
         }
     }
 }
