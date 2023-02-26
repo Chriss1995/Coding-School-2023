730dd6a248b153f31568280e3b567f2d621fa07e
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-26 12:33:21 +0200

    Login in Blazor

---
+++ Session-30/FuelGasStation/FuelGasStation.Blazor/Shared/EmployeeUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FuelGasStation.Model.Enums;
+
+namespace FuelGasStation.Blazor.Shared
+{
+    public class EmployeeUser
+    {
+        public EmployeeType Role { get; set; }
+    }
+}
